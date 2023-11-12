1a5cbcfeb3680264ae58075dede80b7333ba144d
Brady Winslow <97055964+bradywinslow@users.noreply.github.com>
2023-11-11 23:13:25 -0800

    curtain menu, colors, fonts

--- src/components/Header.js
+++ src/components/Header.js
@@ -14,30 +14,31 @@ const navMenu = [
 export default function Header() {
     return (
         <nav className={styles.nav}>
-            <div className={styles.navItems}>
-                <a className={styles.logo} href="./">Brady Winslow</a>
-                <ul className={styles.ul}>
-                    {navMenu.map((item) => (
-                        <a 
-                            href={item.href}
-                        >
-                            {item.name}
-                        </a>
-                    ))}
-                </ul>
-            </div>
-            <div className={styles.contact}>
-                <p>Contact: </p>
-                <a className={styles.FontAwesomeIcon} href="https://www.linkedin.com/in/brady-winslow/" target="_blank" rel="noreferrer">
-                    <FontAwesomeIcon icon={faLinkedinIn} />
-                </a>
-                <a className={styles.FontAwesomeIcon} href="https://github.com/bradywinslow" target="_blank" rel="noreferrer">
-                    <FontAwesomeIcon icon={faGithub} />
-                </a>
-                <a className={styles.FontAwesomeIcon} href="mailto:brady.winslow@gmail.com" target="_blank" rel="noreferrer">
-                    <FontAwesomeIcon icon={faEnvelope} />
-                </a>
-                <CurtainMenu />
+            <div className={styles.navContainer}>
+                <div className={styles.navItems}>
+                    <a className={styles.logo} href="./">BRADY WINSLOW</a>
+                    <ul className={styles.ul}>
+                        {navMenu.map((item) => (
+                            <a 
+                                href={item.href}
+                            >
+                                {item.name}
+                            </a>
+                        ))}
+                    </ul>
+                </div>
+                <div className={styles.contact}>
+                    <a className={styles.FontAwesomeIcon} href="https://www.linkedin.com/in/brady-winslow/" target="_blank" rel="noreferrer">
+                        <FontAwesomeIcon icon={faLinkedinIn} />
+                    </a>
+                    <a className={styles.FontAwesomeIcon} href="https://github.com/bradywinslow" target="_blank" rel="noreferrer">
+                        <FontAwesomeIcon icon={faGithub} />
+                    </a>
+                    <a className={styles.FontAwesomeIcon} href="mailto:brady.winslow@gmail.com" target="_blank" rel="noreferrer">
+                        <FontAwesomeIcon icon={faEnvelope} />
+                    </a>
+                    <CurtainMenu />
+                </div>
             </div>
         </nav>
     );
