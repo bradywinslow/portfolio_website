116e1d61f890e2c8137ed09edd32133633b4042f
Brady Winslow <97055964+bradywinslow@users.noreply.github.com>
2023-11-26 23:20:22 -0800

    Responsive design and menu opening and closing

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import styles from '../styles/Header.module.css';
 import { navMenu } from '../constants/global.js';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -7,9 +7,26 @@ import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
 export default function Header() {
     const [isOpen, setIsOpen] = useState(false);
     
-    const toggle = () => {
+    let menuRef = useRef();
+
+    const toggle = (event) => {
+        event.stopPropagation();
         setIsOpen(!isOpen);
     };
+
+    useEffect(() => {
+        let handler = (event) => {
+            if (!menuRef.current.contains(event.target) && !event.target.classList.contains(styles.faBtnIcons)) {
+                setIsOpen(false);
+            }
+        };
+        
+        document.addEventListener('click', handler);
+
+        return () => {
+            document.removeEventListener('click', handler);
+        }
+    });
 
     return (
         <nav className={styles.nav}>
@@ -17,7 +34,7 @@ export default function Header() {
                 <div className={styles.logoContainer}>
                     <a className={styles.logo} href="./">BRADY WINSLOW</a>
                 </div>
-                <div className={styles.navMenuItemsContainer}>
+                <div className={styles.navMenuItemsContainer} ref={menuRef}>
                     <ul className={`${styles.ul} ${isOpen ? styles.dropdownOpen : ''}`}>
                         {navMenu.map((item) => (
                             <a href={item.href} onClick={toggle}>
@@ -26,7 +43,7 @@ export default function Header() {
                         ))}
                     </ul>
                 </div>
-                <div className={`${styles.faBtnIcons} ${isOpen ? styles.closeIcon : ''}`} onClick={toggle}>
+                <div className={`${styles.faBtnIcons} ${isOpen ? styles.closeIcon : styles.openIcon}`} onClick={toggle}>
                     <FontAwesomeIcon icon={isOpen ? faTimes : faBars} />
                 </div>
             </div>
