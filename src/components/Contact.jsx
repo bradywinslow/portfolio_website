7213965e0af785e99a1455c22d2aa823e5fdf5b7
Brady Winslow <97055964+bradywinslow@users.noreply.github.com>
2024-06-11 23:48:57 -0700

    add aria-label attributes to a tags

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,13 +7,31 @@ import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 export default function Contact() {
     return (
         <div className={styles.contact}>
-            <a className={styles.FontAwesomeIcon} href="https://www.linkedin.com/in/brady-winslow/" target="_blank" rel="noreferrer">
+            <a
+                className={styles.FontAwesomeIcon}
+                href='https://www.linkedin.com/in/brady-winslow/'
+                target='_blank'
+                rel='noreferrer'
+                aria-label='LinkedIn'
+            >
                 <FontAwesomeIcon icon={faLinkedinIn} />
             </a>
-            <a className={styles.FontAwesomeIcon} href="https://github.com/bradywinslow" target="_blank" rel="noreferrer">
+            <a
+                className={styles.FontAwesomeIcon}
+                href='https://github.com/bradywinslow'
+                target='_blank'
+                rel='noreferrer'
+                aria-label='GitHub'
+            >
                 <FontAwesomeIcon icon={faGithub} />
             </a>
-            <a className={styles.FontAwesomeIcon} href="mailto:brady.winslow@gmail.com" target="_blank" rel="noreferrer">
+            <a
+                className={styles.FontAwesomeIcon}
+                href='mailto:brady.winslow@gmail.com'
+                target='_blank'
+                rel='noreferrer'
+                aria-label='Email'
+            >
                 <FontAwesomeIcon icon={faEnvelope} />
             </a>
         </div>
