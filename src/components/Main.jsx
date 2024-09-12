6c8029bd2b8798e0395171e2b50885005f1a9d91
Brady Winslow <97055964+bradywinslow@users.noreply.github.com>
2024-09-11 23:02:17 -0700

    add Reddit Client Gaming Feed to projects section and update skills section

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import styles from '../styles/Main.module.css';
-import aboutPhoto from "../Headshot.jpg";
-import { FaHtml5, FaCss3Alt, FaReact, FaGit, FaGithub } from "react-icons/fa";
-import { SiJavascript, SiChakraui } from "react-icons/si";
+import aboutPhoto from '../Headshot.jpg';
+import { FaHtml5, FaCss3Alt, FaReact, FaGit, FaGithub } from 'react-icons/fa';
+import { SiJavascript, SiTypescript, SiChakraui } from 'react-icons/si';
+import { RiNextjsFill } from 'react-icons/ri';
 
 export default function Main() {
     return (
@@ -54,6 +55,22 @@ export default function Main() {
                             <p>Find Your Hat is a game. After setting the width and height of the playing field, along with the percentage of the field to be covered in holes, a user navigates through the field, trying to avoid the holes to find the hat.</p>
                             <h4><a href='https://github.com/bradywinslow/findYourHat'>GitHub Repo</a></h4>
                         </article>
+                        <article className={styles.projectDetails}>
+                            <h3><a href='https://reddit-client-gaming-feed.vercel.app/'>Reddit Client Gaming Feed</a></h3>
+                            { /* <div className={styles.videoDemo}>
+                                <iframe
+                                    className={styles.video}
+                                    src='https://www.loom.com/embed/e75761eb90f24aefa62a0fc6cfa5dd80?sid=939e91fe-1b77-4e7c-8c9e-e063f2367d4d'
+                                    title='Find Your Hat application video demo'
+                                    frameBorder="0"
+                                    webkitallowfullscreen='true'
+                                    mozallowfullscreen='true'
+                                    allow='fullscreen'
+                                ></iframe>
+                            </div> */ }
+                            <p>Reddit Client Gaming Feed is a web application that displays gaming-related posts from Reddit, providing users with a feed of content focused on gaming discussions, media, and news.</p>
+                            <h4><a href='https://github.com/bradywinslow/reddit-client'>GitHub Repo</a></h4>
+                        </article>
                     </div>
                 </section>
                 <section className={styles.skills}>
@@ -62,7 +79,9 @@ export default function Main() {
                         <FaHtml5 className={styles.icons} />
                         <FaCss3Alt className={styles.icons} />
                         <SiJavascript className={styles.icons} />
+                        <SiTypescript className={styles.icons} />
                         <FaReact className={styles.icons} />
+                        <RiNextjsFill className={styles.icons} />
                         <SiChakraui className={styles.icons} />
                         <FaGit className={styles.icons} />
                         <FaGithub className={styles.icons} />
