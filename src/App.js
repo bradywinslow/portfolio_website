171fb9952139167f77f2ec6f17ffdd722268afcf
Brady Winslow <97055964+bradywinslow@users.noreply.github.com>
2025-05-10 19:30:26 -0700

    change fonts

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import Footer from './components/Footer.jsx';
 export default function App() {
   return (
     <>
-
       <header>
         <Header />
       </header>
@@ -18,7 +17,6 @@ export default function App() {
       <footer>
         <Footer />          
       </footer>
-
     </>
   );
 }
