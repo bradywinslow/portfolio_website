b2971a9590311cf78420bf838ea0272812e3dd5c
Brady Winslow <97055964+bradywinslow@users.noreply.github.com>
2025-05-11 22:26:48 -0700

    update and style about section

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from '../styles/Main.module.css';
-import aboutPhoto from '../Headshot.jpg';
+import AboutSection from '../components/AboutSection.jsx';
 import ContactIcons from './ContactIcons.jsx';
 import ContactForm from './ContactForm.jsx';
 import SkillsCategories from './SkillsCategories.jsx';
@@ -12,26 +12,7 @@ export default function Main() {
                 <section id='about' className={styles.aboutContainer}>
                     <h2 className={styles.sectionHeading}>About</h2>
                     <div className={styles.about}>
-                        <div className={styles.aboutTextAndPhoto}>
-                            <div className={styles.aboutText}>
-                                <p>I have a Ph.D. in history and have worked as a secondary school history teacher and in customer success.</p>
-                                <p>I'm now a front-end web developer. Drawing on my previous roles, I'm devoted to building customer-oriented and user-friendly applications.</p>
-                                <p>I live in the San Francisco Bay Area.</p>
-                            </div>
-                            <div className={styles.headshotContainer}>
-                                <img src={aboutPhoto} alt='Headshot' className={styles.headshot}/>
-                            </div>
-                        </div>
-                        <div className={styles.resumeButton}>
-                            <a href='/Brady_Winslow_Resume.pdf' download target='_blank' className={styles.resumeLink}>
-                                <button
-                                    className={styles.buttons}
-                                    type='button'
-                                >
-                                    Download Resume
-                                </button>
-                            </a>
-                        </div>
+                        <AboutSection />
                     </div>
                 </section>
                 <section id='projects' className={styles.projects}>
