f12889172492ee51a54924bcaf2aaa72c95a8361
Brady Winslow <97055964+bradywinslow@users.noreply.github.com>
2025-05-16 00:34:41 -0700

    clean up main component styles

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -8,16 +8,16 @@ import ContactSection from './ContactSection.jsx';
 export default function Main() {
     return (
         <div className={styles.mainContent}>
-            <section id='about' className={styles.aboutContainer}>
+            <section id='about' className={styles.sectionContainers}>
                 <AboutSection />
             </section>
-            <section id='projects' className={styles.projectsContainer}>
+            <section id='projects' className={styles.sectionContainers}>
                 <ProjectsSection />
             </section>
-            <section id='skills' className={styles.skillsContainer}>
+            <section id='skills' className={styles.sectionContainers}>
                 <SkillsSection />
             </section>
-            <section id='contact' className={styles.contactContainer}>
+            <section id='contact' className={styles.sectionContainers}>
                 <ContactSection />
             </section>
         </div>
