af7d1b73b0982756ebcd1a851b80acb93ea40034
Brady Winslow <97055964+bradywinslow@users.noreply.github.com>
2025-06-17 22:59:57 -0700

    update skills section on Home page

--- src/constants/skillsCardsInfo.ts
+++ src/constants/skillsCardsInfo.ts
@@ -1,5 +1,5 @@
 import { FaReact } from 'react-icons/fa';
-import { SiTypescript, SiTailwindcss, SiChakraui, SiJest, SiTestinglibrary } from 'react-icons/si';
+import { SiTypescript, SiTailwindcss, SiChakraui, SiJest, SiReactrouter, SiTestinglibrary } from 'react-icons/si';
 import { RiNextjsFill } from 'react-icons/ri';
 
 const skillsCardsInfo = [
@@ -9,6 +9,7 @@ const skillsCardsInfo = [
             { name: 'Next.js', icon: RiNextjsFill },
             { name: 'TypeScript', icon: SiTypescript },
             { name: 'Jest', icon: SiJest },
+            { name: 'React Router', icon: SiReactrouter },
             { name: 'Chakra UI', icon: SiChakraui },
             { name: 'Tailwind CSS', icon: SiTailwindcss },
             { name: 'React Testing Library', icon: SiTestinglibrary }
