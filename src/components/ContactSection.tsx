1a7f5dfd43507f06561db08a447d31021fbf85d5
Brady Winslow <97055964+bradywinslow@users.noreply.github.com>
2025-06-21 13:59:39 -0700

    update styling of contact page

--- src/components/ContactSection.tsx
+++ src/components/ContactSection.tsx
@@ -1,12 +1,10 @@
 import styles from '../styles/ContactSection.module.css';
-import SectionHeading from './SectionHeading';
 import ContactForm from './ContactForm';
 import ContactIcons from './ContactIcons';
 
 export default function ContactSection() {
     return (
         <div className={styles.contactSection}>
-            <SectionHeading>Contact</SectionHeading>
                 <div className={styles.contactForm}>
                     <ContactForm />
                 </div>
