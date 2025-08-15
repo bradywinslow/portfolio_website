ef6f672fc03be3f5632ccbc57e06d99044c355f9
Brady Winslow <97055964+bradywinslow@users.noreply.github.com>
2025-08-15 15:39:31 -0700

    update project cards

--- src/components/ProjectsSection.tsx
+++ src/components/ProjectsSection.tsx
@@ -10,53 +10,53 @@ export default function ProjectsSection() {
             <div className={styles.projectsCardContainer}>
                 {projectsCardsInfo.map((cardItem, index) => (
                     <article key={index} className={styles.projectsCard}>
-                    <div className={styles.videoContainer}>
-                        <LiteYouTubeEmbed
-                            id={cardItem.videoId}
-                            adNetwork={false}
-                            playlist={false}
-                            poster='hqdefault'
-                            title={cardItem.videoTitle}
-                            cookie={false}
-                            playerClass={styles.youtubePlayButton}
-                        />
-                    </div>
-                    <div className={styles.projectsCardTitle}>
-                        <h3 className={styles.projectsCardLink}>
-                            <a
-                                href={cardItem.projectHref}
-                                target='_blank'
-                                rel='noopener noreferrer'
-                            >
-                                {cardItem.projectTitle}
-                            </a>
-                        </h3>
-                    </div>
-                    <div className={styles.projectsSkills}>
-                        <ul>
-                            {cardItem.projectSkills.map((item, id) => (
-                                <li key={id}>
-                                    <item.icon className={styles.projectsSkillsIcons} />
-                                    {item.skill}
-                                </li>
-                            ))}
-                        </ul>                        
-                    </div>
-                    <div className={styles.projectsCardText}>
-                        <p>{cardItem.projectText}</p>
-                    </div>
-                    <a
-                        className={styles.githubRepoLink}
-                        href={cardItem.githubRepoHref}
-                        target='_blank'
-                        rel='noopener noreferrer'
-                    >
-                        <Button
-                            type='button'
+                        <div className={styles.videoContainer}>
+                            <LiteYouTubeEmbed
+                                id={cardItem.videoId}
+                                adNetwork={false}
+                                playlist={false}
+                                poster='hqdefault'
+                                title={cardItem.videoTitle}
+                                cookie={false}
+                                playerClass={styles.youtubePlayButton}
+                            />
+                        </div>
+                        <div className={styles.projectsCardTitle}>
+                            <h3 className={styles.projectsCardLink}>
+                                <a
+                                    href={cardItem.projectHref}
+                                    target='_blank'
+                                    rel='noopener noreferrer'
+                                >
+                                    {cardItem.projectTitle}
+                                </a>
+                            </h3>
+                        </div>
+                        <div className={styles.projectsSkills}>
+                            <ul>
+                                {cardItem.projectSkills.map((item, id) => (
+                                    <li key={id}>
+                                        <item.icon className={styles.projectsSkillsIcons} />
+                                        {item.skill}
+                                    </li>
+                                ))}
+                            </ul>                        
+                        </div>
+                        <div className={styles.projectsCardText}>
+                            <p>{cardItem.projectText}</p>
+                        </div>
+                        <a
+                            className={styles.githubRepoLink}
+                            href={cardItem.githubRepoHref}
+                            target='_blank'
+                            rel='noopener noreferrer'
                         >
-                            Code
-                        </Button>
-                    </a>
+                            <Button
+                                type='button'
+                            >
+                                Code
+                            </Button>
+                        </a>
                     </article>
                 ))}
             </div>
--- src/constants/projectsCardsInfo.ts
+++ src/constants/projectsCardsInfo.ts
@@ -1,9 +1,44 @@
 import { FaReact } from 'react-icons/fa';
-import { SiTypescript, SiTailwindcss, SiChakraui, SiJest, SiReactrouter, SiTestinglibrary, SiVercel, SiJavascript, SiNetlify } from 'react-icons/si';
+import { SiTypescript, SiTailwindcss, SiChakraui, SiJest, SiReactrouter, SiTestinglibrary, SiVitest, SiJavascript } from 'react-icons/si';
 import { RiNextjsFill } from 'react-icons/ri';
 import { TbBrandOauth } from "react-icons/tb";
 
 const projectsCardsInfo = [
+    {
+        videoSrc: '',
+        videoId: '',
+        videoTitle: 'Genre Identifier application video demo',
+        projectHref: 'https://genre-identifier.vercel.app/',
+        projectTitle: 'Genre Identifier',
+        projectText: 'Genre Identifier is a web application that allows a user to search artists in the Spotify music catalog and displays information about the artist and their genre(s).',
+        projectSkills: [
+            {
+                skill: 'React',
+                icon: FaReact
+            },
+            {
+                skill: 'TypeScript',
+                icon: SiTypescript
+            },
+            {
+                skill: 'React Router',
+                icon: SiReactrouter
+            },
+            {
+                skill: 'OAuth 2.0',
+                icon: TbBrandOauth
+            },
+            {
+                skill: 'Tailwind CSS',
+                icon: SiTailwindcss
+            },
+            {
+                skill: 'Vitest',
+                icon: SiVitest
+            }
+        ],
+        githubRepoHref: 'https://github.com/bradywinslow/genre-identifier',
+    },
     {
         videoSrc: 'https://www.youtube.com/embed/Ixs2bfRP0Sg?si=7YlVVqlX9ZjWJQMx',
         videoId: 'Ixs2bfRP0Sg',
@@ -13,20 +48,20 @@ const projectsCardsInfo = [
         projectText: 'Reddit Client Gaming Feed is a web application that displays gaming-related posts from Reddit, providing users with a feed of content focused on gaming discussions, media, and news.',
         projectSkills: [
             {
-                skill: 'TypeScript',
-                icon: SiTypescript
-            },
-            {
                 skill: 'Next.js',
                 icon: RiNextjsFill
+            },
+            {
+                skill: 'TypeScript',
+                icon: SiTypescript
             },
             {
                 skill: 'Chakra UI',
                 icon: SiChakraui
             },
             {
-                skill: 'Vercel',
-                icon: SiVercel
+                skill: 'Jest',
+                icon: SiJest
             }
         ],
         githubRepoHref: 'https://github.com/bradywinslow/reddit-client',
@@ -40,12 +75,12 @@ const projectsCardsInfo = [
         projectText: 'Find Your Hat is a game. After setting a width and height, and a percentage for holes to appear on the field, a user navigates through the field, trying to avoid the holes to find the hat.',
         projectSkills: [
             {
-                skill: 'JavaScript',
-                icon: SiJavascript
-            },
-            {
                 skill: 'React',
                 icon: FaReact
+            },
+            {
+                skill: 'JavaScript',
+                icon: SiJavascript
             },
             {
                 skill: 'React Router',
@@ -54,10 +89,6 @@ const projectsCardsInfo = [
             {
                 skill: 'Jest',
                 icon: SiJest
-            },
-            {
-                skill: 'Netlify',
-                icon: SiNetlify
             }
         ],
         githubRepoHref: 'https://github.com/bradywinslow/findYourHat',
@@ -71,12 +102,12 @@ const projectsCardsInfo = [
         projectText: 'Jammming is a web application that allows a user to sign in to their Spotify account, search for songs, albums, or artists, create a personalized playlist, and save the playlist to Spotify.',
         projectSkills: [
             {
-                skill: 'JavaScript',
-                icon: SiJavascript
-            },
-            {
                 skill: 'React',
                 icon: FaReact
+            },
+            {
+                skill: 'JavaScript',
+                icon: SiJavascript
             },
             {
                 skill: 'React Router',
@@ -89,10 +120,6 @@ const projectsCardsInfo = [
             {
                 skill: 'Chakra UI',
                 icon: SiChakraui
-            },
-            {
-                skill: 'Netlify',
-                icon: SiNetlify
             }
         ],
         githubRepoHref: 'https://github.com/bradywinslow/jammming',
